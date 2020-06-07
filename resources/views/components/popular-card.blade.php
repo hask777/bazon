a38a1708741ca9ec06b1cea88fd2bc2b69e9b9b8
hask777 <haskellisp@gmail.com>
2020-06-07 11:46:17 +0300

    'hh'

--- resources/views/components/popular-card.blade.php
+++ resources/views/components/popular-card.blade.php
@@ -31,7 +31,7 @@
             </div>     
         </div>
     </a>
-    <div class="flex justify-between px-2 py-2 text-sm">
+    <div class="flex justify-between px-2 py-2 text-xs sm:text-xs md:text-base">
         <h2 class="title">{{mb_strimwidth($movie['title'], 0, 18, "...")}}</h2>
         @if(!empty($movie['release_date']))
             <span>{{\Carbon\Carbon::parse($movie['release_date'])->format('Y') }}</span>

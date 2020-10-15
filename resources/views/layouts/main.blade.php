266b958efcbe6dd61623e4a067d7550dc3eea8df
hask777 <haskellisp@gmail.com>
2020-10-15 14:29:30 +0300

    'ff'

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,15 @@
     <meta name="description" content="">
     <meta name=”robots” content=”index, follow”>
 
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'KinoZET') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
     @php
         $current_uri = request()->segments();
         // dump($current_uri);    
@@ -33,8 +42,6 @@
 
     <link rel="canonical" href="http://example.com/" />
     
-    <title>KinoZet</title>
-
     <link rel="stylesheet" href="{{secure_asset('css/main.css')}}">
     <link rel="stylesheet" href="{{secure_asset('css/app.css')}}">
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
@@ -50,6 +57,59 @@
     </head>
     <body class="bg-gray-900 text-white">
        
+        <div id="app">
+            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+    
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav mr-auto">
+    
+                        </ul>
+    
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                            @guest
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }}
+                                    </a>
+    
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                                         document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+    
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            @endguest
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+    
+        </div>
 
         <header class="border-b border-gray-800">
             <div class="header_wrapper container mx-auto  px-4 py-4 flex">
@@ -122,6 +182,7 @@
   
         </div>
 
+        <!-- Scripts -->
         
         <script type="text/javascript" src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js'> </script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/5.4.5/js/swiper.min.js"></script>
@@ -132,7 +193,8 @@
         <livewire:scripts>
 
        
-       
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
         <script type="text/javascript" src="{{asset('js/main.js')}}"></script>
         <script type="text/javascript" src="{{asset('js/swiper.js')}}"></script>
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -19,4 +19,5 @@ mix.js('resources/js/app.js', 'public/js')
 
 mix.sass('resources/sass/app.scss', 'public/css');
 
-mix.browserSync('warm-plateau-75468.herokuapp.com');
+// mix.browserSync('warm-plateau-75468.herokuapp.com');
+mix.browserSync('bazon.test');
